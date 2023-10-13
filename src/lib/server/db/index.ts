8dfcd231de239b8eb471a65f06f7067eef2ceba1
Dallen Baldwin <dallen.baldwin@gmail.com>
2023-10-12 22:11:10 -0600

    don't need to export stuff so much

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,7 +1,6 @@
 import { drizzle } from 'drizzle-orm/bun-sqlite'
 import { Database } from 'bun:sqlite'
 import { schema } from './schema'
-export { eq, lt, gte, ne, sql } from 'drizzle-orm'
 
 const sqlite = new Database('sqlite.db')
 export const db = drizzle(sqlite, { schema })
