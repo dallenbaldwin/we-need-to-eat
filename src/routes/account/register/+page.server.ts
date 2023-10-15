f1364c56a04e326c869e2eaaf5e87926d83738c9
Dallen Baldwin <dallen.baldwin@gmail.com>
2023-10-15 10:12:12 -0600

    test

--- src/routes/account/register/+page.server.ts
+++ src/routes/account/register/+page.server.ts
@@ -1,10 +1,10 @@
 import { fail, redirect, type Actions } from '@sveltejs/kit'
-import { eq } from 'drizzle-orm'
-import { db, users } from '$lib/server/db'
+import { auth } from '$lib/server/auth'
+import { DrizzleError } from 'drizzle-orm'
 
 export const actions: Actions = {
-  default: async ({ request }) => {
-    // TODO follow lucia
+  default: async ({ request, locals }) => {
+    // TODO test flow
     const form = await request.formData()
     const username = form.get('username')
     const password = form.get('password')
@@ -12,23 +12,35 @@ export const actions: Actions = {
 
     if (!username || typeof username !== 'string')
       return fail(400, { reason: 'invalid username' })
+    if (!username.length)
+      return fail(400, { reason: 'usernames cannot be empty' })
     if (!password || typeof password !== 'string')
       return fail(400, { reason: 'invalid password' })
+    if (password.length < 6)
+      return fail(400, { reason: 'passwords must be at least 6 characters' })
     if (!confirm || typeof confirm !== 'string')
       return fail(400, { reason: 'invalid confirm' })
     if (password !== confirm)
       return fail(400, { reason: 'passwords must match' })
 
     try {
-      const user = await db.query.users.findFirst({
-        where: (users) => eq(users.username, username),
+      // create user with username method
+      const user = await auth.createUser({
+        key: {
+          providerId: 'username',
+          password,
+          // lowercase to ensure uniqueness
+          providerUserId: username.toLowerCase(),
+        },
+        attributes: { username, admin: false },
       })
-      if (user) return fail(400, { reason: 'username already exists' })
-      await db.insert(users).values({
-        username,
-        id: crypto.randomUUID(),
+      const session = await auth.createSession({
+        userId: user.id,
+        attributes: {},
       })
+      locals.auth.setSession(session)
     } catch (err) {
+      if (err instanceof DrizzleError) return fail(500, { reason: err.message })
       return fail(500, {
         reason: err instanceof Error ? err.message : String(err),
       })
