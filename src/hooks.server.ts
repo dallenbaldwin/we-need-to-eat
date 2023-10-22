ebd9e9d63c08da198847a51afc23187c03940236
Dallen Baldwin <dallen.baldwin@gmail.com>
2023-10-22 15:52:10 -0600

    fix issue with redirect

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,10 +4,11 @@ import { auth } from '$lib/server'
 export const handle: Handle = async ({ event, resolve }) => {
   const handledRequest = auth.handleRequest(event)
   const session = await handledRequest.validate()
-  if (!session || protect(event.url.pathname)) throw redirect(302, '/account')
+  if (!session && protect(event.url.pathname)) throw redirect(302, '/account')
 
   event.locals.auth = handledRequest
-  event.locals.user = await auth.getUser(session.user.id)
+  if (session) event.locals.user = await auth.getUser(session.user.id)
+
   return resolve(event)
 }
 
