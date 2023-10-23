fb8f67c25fcf81b5c1ed60a3de7ca4ea919b9150
Dallen Baldwin <dallen.baldwin@gmail.com>
2023-10-22 23:32:58 -0600

    dynamic favicon later?

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,14 @@
   import '../app.css'
   import Navbar from '$lib/components/Navbar.svelte'
   import type { LayoutData } from './$types'
+  import favicon from '$lib/assets/favicon/default.png'
 
   export let data: LayoutData
 </script>
 
+<svelte:head>
+  <link rel="icon" href={favicon} />
+</svelte:head>
 <Navbar user={data?.user} />
 <div class="p-6">
   <slot />
